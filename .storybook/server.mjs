63ed83ae9ef4778a2907d6eef551e6e566b87a65
Alexander Bertling <alexander-bertling@web.de>
2022-03-07 12:57:20 +0100

    complete separation of storybook-server-config

--- .storybook/server.mjs
+++ .storybook/server.mjs
@@ -1,12 +1,10 @@
 import { storybookPlugin } from '@web/dev-server-storybook';
-import baseConfig from '../web-dev-server.config.mjs';
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
-  //...baseConfig,
   open: '/',
   /** Resolve bare module imports */
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
-  plugins: [storybookPlugin({ type: 'web-components' }), ...baseConfig.plugins],
+  plugins: [storybookPlugin({ type: 'web-components' })]
 });
