fcc3cacd6241d34e7be1c09bc25514a1374de7af
Alexander Bertling <alexander-bertling@web.de>
2022-03-07 15:44:55 +0100

    Fifth Try for Caching wrong unpkg urls

--- rollup.config.js
+++ rollup.config.js
@@ -33,7 +33,7 @@ const baseConfig = createSpaConfig({
     ]*/
     runtimeCaching: [
       {
-        urlPattern: new RegExp('.*unpkg.*'),
+        urlPattern: "/",
         handler: 'CacheFirst',
       },
     ],
